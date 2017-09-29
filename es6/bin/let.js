ebd95f49041346a3b60b58a5c7f4ebcf40a797e4
ybluesky <xjz1688@163.com>
2017-09-29 17:09:30 +0800

    es6 let

--- es6/bin/let.js
+++ es6/bin/let.js
@@ -67,7 +67,24 @@
 /* 0 */
 /***/ (function(module, exports) {
 
+//es5时，只支持全局作用域、函数作用域
+var a5 = [];
+for (var i = 0; i < 10; i++) {
+    a5[i] = function () {
+        console.log(i);
+    };
+}
+a5[6](); // 10
 
+
+//es6 支持块级作用域
+var a6 = [];
+for (let i = 0; i < 10; i++) {
+    a6[i] = function () {
+        console.log(i);
+    };
+}
+a6[6](); // 6
 
 /***/ })
 /******/ ]);
